6638f8e1d44342c5e1e5934987e32cad8bb2f347
Maharjan Binish <binish@fenrir.co.jp>
2022-02-19 18:41:05 +0900

    present sample view controller as first view controller

--- App/Storyboard/GithubViewer-Storyboard/ViewController.swift
+++ App/Storyboard/GithubViewer-Storyboard/ViewController.swift
@@ -6,13 +6,7 @@
 //
 
 import UIKit
+import DIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController { }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
