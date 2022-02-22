54e1a1e8c8b7e82e544454ae3e427b364aace1d0
Maharjan Binish <binish@fenrir.co.jp>
2022-02-22 16:01:57 +0900

    added dikit to each module and delete sample viewcontroller

---
+++ App/Storyboard/Domain/Sources/Domain/Resource/DomainResolver.swift
@@ -0,0 +1,22 @@
+//
+//  DomainResolver.swift
+//  
+//
+//  Created by Maharjan Binish on 2022/02/22.
+//
+
+import Foundation
+import DIKit
+
+public protocol DomainResolver: Resolver {
+    func provideDomainModel() -> DomainModel
+}
+
+public final class DomainResolverImpl: DomainResolver {
+    
+    public init() { }
+    
+    public func provideDomainModel() -> DomainModel {
+        return DomainModel(string: "Hello Domain!")
+    }
+}
