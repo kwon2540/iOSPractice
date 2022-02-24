7203187716bd4c075adb4047153b6a5e66c24a9f
Maharjan Binish <binish@fenrir.co.jp>
2022-02-24 16:02:34 +0900

    add common protocol for github requests

---
+++ App/Storyboard/Data/Sources/Data/Network/APIRequest/GitHubAPIRequest/GitHubAPIRequest.swift
@@ -0,0 +1,15 @@
+//
+//  GitHubAPIRequest.swift
+//  
+//
+//  Created by Maharjan Binish on 2022/02/24.
+//
+
+import Foundation
+
+public protocol GitHubAPIRequest: APIRequest {
+}
+
+public extension GitHubAPIRequest {
+    var baseURL: URL { URL(string: "https://api.github.com")! }
+}
