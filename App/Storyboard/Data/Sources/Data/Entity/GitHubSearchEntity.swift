fedb2d97b725a964177c2cd3780abe7949d83566
Maharjan Binish <binish@fenrir.co.jp>
2022-02-24 17:01:26 +0900

    add mapper method to entity

--- App/Storyboard/Data/Sources/Data/Entity/GitHubSearchEntity.swift
+++ App/Storyboard/Data/Sources/Data/Entity/GitHubSearchEntity.swift
@@ -11,6 +11,9 @@ import Domain
 public struct GitHubSearchEntity: Decodable {
     public let items: [GitHubRepositoryEntity]
     
+    func toModel() -> GitHubSearchModel {
+        GitHubSearchModel(items: items.map { $0.toModel() })
+    }
 }
 
 public struct GitHubRepositoryEntity: Decodable {
@@ -23,6 +26,16 @@ public struct GitHubRepositoryEntity: Decodable {
     public let language: String?
     public let stargazersCount: Int
     
+    func toModel() -> GitHubRepositoryModel {
+        GitHubRepositoryModel(id: String(id),
+                         name: name,
+                         fullName: fullName,
+                         owner: owner.toModel(),
+                         htmlUrl: htmlUrl,
+                         description: description,
+                         language: language,
+                         stargazersCount: String(stargazersCount))
+    }
 }
 
 public struct GitHubRepositoryOwnerEntity: Decodable {
@@ -30,4 +43,7 @@ public struct GitHubRepositoryOwnerEntity: Decodable {
     public let login: String
     public let avatarUrl: URL
     
+    func toModel() -> GitHubRepositoryOwnerModel {
+        GitHubRepositoryOwnerModel(id: String(id), login: login, avatarUrl: avatarUrl)
+    }
 }
