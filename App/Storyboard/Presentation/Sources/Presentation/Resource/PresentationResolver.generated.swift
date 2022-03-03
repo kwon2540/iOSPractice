e9f2b1ea7e03839d31f21245cddc7b9d33aab6b9
Maharjan Binish <binish@fenrir.co.jp>
2022-03-03 17:22:36 +0900

    DIKit autoGenereted code for viewController and viewModels

--- App/Storyboard/Presentation/Sources/Presentation/Resource/PresentationResolver.generated.swift
+++ App/Storyboard/Presentation/Sources/Presentation/Resource/PresentationResolver.generated.swift
@@ -5,8 +5,27 @@
 
 import DIKit
 import Foundation
+import UIKit
 
 extension PresentationResolver {
+
+    func resolveDetailViewController() -> DetailViewController {
+        let detailViewModel = resolveDetailViewModel()
+        return DetailViewController(dependency: .init(viewModel: detailViewModel))
+    }
+
+    func resolveDetailViewModel() -> DetailViewModel {
+        return DetailViewModel(dependency: .init())
+    }
+
+    func resolveListViewController() -> ListViewController {
+        let listViewModel = resolveListViewModel()
+        return ListViewController(dependency: .init(viewModel: listViewModel))
+    }
+
+    func resolveListViewModel() -> ListViewModel {
+        return ListViewModel(dependency: .init())
+    }
 
 }
 
