5c42ff0704ad8fc0b76679dcd8b44de25f2611d7
Maharjan Binish <binish@fenrir.co.jp>
2022-03-03 17:21:58 +0900

    add DetailViewController and DetailViewModel

---
+++ App/Storyboard/Presentation/Sources/Presentation/Screen/Detail/DetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailViewController.swift
+//  
+//
+//  Created by Maharjan Binish on 2022/03/03.
+//
+
+import Foundation
+import DIKit
+
+public final class DetailViewController: BaseViewController<DetailViewModel>, Injectable {
+    
+    public struct Dependency {
+        let viewModel: DetailViewModel
+    }
+    
+    public init(dependency: Dependency) {
+        super.init(nibName: nil, bundle: nil)
+        self.viewModel = dependency.viewModel
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder: ) has not been implemented")
+    }
+}
+
+
---
+++ App/Storyboard/Presentation/Sources/Presentation/Screen/Detail/DetailViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewModel.swift
+//  
+//
+//  Created by Maharjan Binish on 2022/03/03.
+//
+
+import Foundation
+import DIKit
+
+public class DetailViewModel: BaseViewModel, Injectable {
+    
+    public struct Dependency {}
+    
+    required public init(dependency: Dependency) { }
+}
