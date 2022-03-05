8d304dde1bc7a948036bfbc4418e66fb85b52e84
Maharjan Binish <binish@fenrir.co.jp>
2022-03-05 15:02:32 +0900

    add listView

---
+++ App/Storyboard/Presentation/Sources/Presentation/Screen/List/ListView.swift
@@ -0,0 +1,15 @@
+//
+//  ListView.swift
+//  
+//
+//  Created by Maharjan Binish on 2022/03/05.
+//
+
+import UIKit
+
+final class ListView: UIView {
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+    }
+}
