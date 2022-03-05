a9019238cd24a082f5d36c62bd271c5d2ebd3488
Maharjan Binish <binish@fenrir.co.jp>
2022-03-05 13:30:23 +0900

    add a enum for the loadingState

---
+++ App/Storyboard/Presentation/Sources/Presentation/Common/LoadingState.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingState.swift
+//  
+//
+//  Created by Maharjan Binish on 2022/03/05.
+//
+
+import Foundation
+
+public enum LoadingState {
+    case initial
+    case loading
+    case completed
+}
