bde736ef90c4d0aeed2866d11cc6514bc2681664
Maharjan Binish <binish@fenrir.co.jp>
2022-03-05 15:02:55 +0900

    show listView as mainView for listViewController

--- App/Storyboard/Presentation/Sources/Presentation/Screen/List/ListViewController.swift
+++ App/Storyboard/Presentation/Sources/Presentation/Screen/List/ListViewController.swift
@@ -22,5 +22,9 @@ public final class ListViewController: BaseViewController<ListViewModel>, Inject
     public required init?(coder: NSCoder) {
         fatalError("init(coder: ) has not been implemented")
     }
+    
+    public override func loadView() {
+        view = ListView()
+    }
 }
 
