30bffc0da146b47de0d1ec166365d91d58215cf5
Maharjan Binish <binish@fenrir.co.jp>
2022-04-08 11:23:55 +0900

    add extension to register and dequeue tableview

---
+++ App/Storyboard/Presentation/Sources/Presentation/Extension/UITableVIew+Extension.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maharjan Binish on 2022/04/08.
+//
+
+import UIKit
+
+extension UITableView {
+  
+  func registerXib<T:UITableViewCell>(of cellClass: T.Type) {
+    let className = cellClass.className
+    let nib: UINib? = UINib(nibName: className, bundle: Bundle.module)
+    register(nib, forCellReuseIdentifier: className)
+  }
+  
+  func dequeueCell<T:UITableViewCell>(of cellClass: T.Type, for indexPath: IndexPath) -> T {
+    let className = cellClass.className
+    guard let cell = dequeueReusableCell(withIdentifier: className, for: indexPath) as? T else {
+      fatalError()
+    }
+    
+    return cell
+  }
+}
