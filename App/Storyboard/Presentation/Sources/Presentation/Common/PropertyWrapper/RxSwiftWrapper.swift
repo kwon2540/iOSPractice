3a0f3b201ca52bf206e8aeb232f89667d317ae16
Maharjan Binish <binish@fenrir.co.jp>
2022-04-10 12:32:26 +0900

    change the type of projected value in @PublishSubjectAsObserver

--- App/Storyboard/Presentation/Sources/Presentation/Common/PropertyWrapper/RxSwiftWrapper.swift
+++ App/Storyboard/Presentation/Sources/Presentation/Common/PropertyWrapper/RxSwiftWrapper.swift
@@ -88,7 +88,7 @@ public struct PublishSubjectAsObserver<T> {
         subject = PublishSubject<T>()
     }
     
-    public var projectedValue: Observable<T> {
+    public var projectedValue: PublishSubject<T> {
         subject
     }
     
