991ded46407d7124530bd6cda15f771542829a83
Maharjan Binish <binish@fenrir.co.jp>
2022-05-29 13:12:40 +0900

    writing test for githubSearchRepository

--- App/Storyboard/Data/Tests/DataTests/DefaultGitHubRepositoryTests.swift
+++ App/Storyboard/Data/Tests/DataTests/DefaultGitHubRepositoryTests.swift
@@ -12,47 +12,47 @@ import RxSwift
 final class DefaultGitHubRepositoryTests: XCTestCase {
     
     func testSearch_callsSearchAPIWithParameters() {
+        // given
+        let apiClient = APIClientStub()
+        let repository = DefaultGitHubRepository(dependency: .init(apiClient: apiClient))
+        let keyword = "swift"
         
-        let apiClient = DefaultAPIClient(dependency: .init())
+        // when
+        repository.fetch(keyword: keyword)
+            .subscribe(onSuccess: { _ in })
+            .dispose()
         
+        // then
+        let expectedHost = "api.github.com"
+        let actualHost = apiClient.urlRequest.url?.host
+        XCTAssertEqual(expectedHost, actualHost)
+        
+        let expectedPath = "/search/repositories"
+        let actualPath = apiClient.urlRequest.url?.path
+        XCTAssertEqual(expectedPath, actualPath)
+
+        let expectedMethod = "GET"
+        let actualMethod = apiClient.urlRequest.httpMethod
+        XCTAssertEqual(expectedMethod, actualMethod)
+        
+        let components = URLComponents(url: apiClient.urlRequest.url!, resolvingAgainstBaseURL: false)
+        let query = components?.queryItems?.first(where: { $0.name == "q" })
+        let expectedQueryValue = keyword
+        let actualQueryValue = query?.value
+        XCTAssertEqual(expectedQueryValue, actualQueryValue)
     }
 }
 
-
 final class APIClientStub: APIClient {
-    func request<Request>(request: Request) -> Single<Request.Response> where Request : APIRequest {
-        fatalError()
-    }
-    
+
     var urlRequest: URLRequest!
     
-    func request<GitHubSearchAPIRequest>(request: GitHubSearchAPIRequest) -> Single<GitHubSearchEntity> {
-        return Single<GitHubSearchEntity>.create { single in
-            single(.success(GitHubSearchEntity.dummy))
-            
+    func requestForData<Request>(request: Request) -> Single<Data> where Request : APIRequest {
+        urlRequest = request.urlRequest
+        
+        return Single.create { single in
+            single(.success(Data()))
             return Disposables.create()
         }
     }
 }
-
-extension GitHubSearchEntity {
-    
-    static var dummy: GitHubSearchEntity {
-        GitHubSearchEntity(items: [])
-    }
-}
-
-
-//
-//
-//@testable import Alamofire
-//@testable import LetsGitHubSearch
-//
-//final class SessionManagerStub: SessionManagerProtocol {
-//  var requestParameters: (url: URLConvertible, method: HTTPMethod, parameters: Parameters?)?
-//
-//  func request(_ url: URLConvertible, method: HTTPMethod, parameters: Parameters?, encoding: ParameterEncoding, headers: HTTPHeaders?) -> DataRequest {
-//    self.requestParameters = (url, method, parameters)
-//    return DataRequest(session: URLSession(), requestTask: .data(nil, nil))
-//  }
-//}
