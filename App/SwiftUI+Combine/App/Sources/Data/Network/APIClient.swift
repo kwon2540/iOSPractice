38dda6db1783fcddf4aefd90c3c9cf66ebbff6a0
j.kwon <j.kwon@fenrir.co.jp>
2022-07-23 12:16:43 +0900

    APIClient作成

---
+++ App/SwiftUI+Combine/App/Sources/Data/Network/APIClient.swift
@@ -0,0 +1,44 @@
+//
+//  APIClient.swift
+//  
+//
+//  Created by クォン ジュンヒョク on 2022/07/16.
+//
+
+import Foundation
+
+public enum APIError: Error {
+    case invalidURL
+    case responseError
+    case parseError(Error)
+}
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+public protocol APIClientProtocol {
+    
+    func perform<T: Decodable>(method: HTTPMethod, url: String?) async throws -> T
+}
+
+public class APIClient: APIClientProtocol {
+    
+    public init() {}
+
+    public func perform<T: Decodable>(method: HTTPMethod, url: String?) async throws -> T {
+        guard let urlString = url, let url = URL(string: urlString) else {
+            throw APIError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    }
+}
